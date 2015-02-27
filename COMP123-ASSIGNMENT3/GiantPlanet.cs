594492e5c59932c6558fd510528e387d7af530cc
RaveenaTayal <ravhome123@gmail.com>
2015-02-27 15:24:35 -0500

    Added GiantPlanet and Terrestrial planet sub classes

---
+++ COMP123-ASSIGNMENT3/GiantPlanet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_ASSIGNMENT3
+{
+
+    //Creating GiantPlanet subclass that inherits from Planet base class and implements IHasMoons and IHasRings interfaces.
+    class GiantPlanet:Planet,IHasMoons,IHasRings
+    {
+
+        //PRIVATE INSTANCE VARIABLES--------------------------------------------------------------------
+        private string _type;
+
+        //CONSTRUCTORS----------------------------------------------------------------------------------
+        public GiantPlanet(string name,double diameter,double mass,string type)
+            :base(name,diameter,mass)
+        {
+
+            //Assign local variable to instance variable------------------------------------------------
+            this._type = type;
+        }
+
+        //PUBLIC METHODS--------------------------------------------------------------------------------
+        public bool HasMoons()
+        {
+            //Check whether the MoonCount is greater than 0 and display true otherwise display false----
+            return (base.MoonCount > 0) ? true : false;
+        }
+
+        public bool HasRings()
+        {
+            //Check whether the RingCount is greater than 0 and display true otherwise display false----
+            return (base.RingCount > 0) ? true : false;
+        }
+    }
+}
---
+++ COMP123-ASSIGNMENT3/TerrestrialPlanet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_ASSIGNMENT3
+{
+
+    //Creating GiantPlanet subclass that inherits from Planet base class and implements IHasMoons and IHasRings interfaces.
+    class TerrestrialPlanet : Planet, IHasMoons, IHabitable
+    {
+
+        //PRIVATE INSTANCE VARIABLES--------------------------------------------------------------------
+        private bool _oxygen;
+
+        //CONSTRUCTORS----------------------------------------------------------------------------------
+        public TerrestrialPlanet(string name,double diameter,double mass,bool oxygen)
+            :base(name,diameter,mass)
+        {
+
+            //Assign local variable to instance variable------------------------------------------------
+            this._oxygen = oxygen;
+        }
+
+        //PUBLIC METHODS--------------------------------------------------------------------------------
+        public bool HasMoons()
+        {
+
+            //Check whether the MoonCount is greater than 0 and display true otherwise display false----
+            return (base.MoonCount > 0) ? true : false;
+        }
+        public bool Habitable()
+        {
+
+            //Check whether the oxygen variable holds true or false ------------------------------------
+            return (this._oxygen) ? true : false;
+        }
+
+
+    }
+}
