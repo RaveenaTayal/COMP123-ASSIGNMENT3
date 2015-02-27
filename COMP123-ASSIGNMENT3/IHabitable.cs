41e605784e9dc5da7cd1431b73c8c01f40fac60d
RaveenaTayal <ravhome123@gmail.com>
2015-02-27 15:06:29 -0500

    Added Planet class, IHasMoons, IHasRings and IHabitable interfaces to the basic C# project

---
+++ COMP123-ASSIGNMENT3/IHabitable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_ASSIGNMENT3
+{
+    // Interface IHabitable -------------------------------------------------------
+    public interface IHabitable
+    {
+
+        //Method header that returns bool datatype---------------------------------
+        bool Habitable();
+    }
+}
---
+++ COMP123-ASSIGNMENT3/IHasRings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_ASSIGNMENT3
+{
+    // Interface IHasRings --------------------------------------------------------
+    public interface IHasRings
+    {
+
+        //Method header that returns bool datatype---------------------------------
+        bool HasRings();
+    }
+}
